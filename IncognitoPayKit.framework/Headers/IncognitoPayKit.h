c88bd265ac0a1ec4bd2ead7e0d4a18a210fd1876
Andreas Reuter <andreas@ndncmnky.com>
2021-03-21 12:52:59 +0100

    Adjust set up to be ready for upload to App Store Connect

---
+++ IncognitoPayKit.framework/Headers/IncognitoPayKit.h
@@ -0,0 +1,19 @@
+//
+//  IncognitoPayKit.h
+//  IncognitoPayKit
+//
+//  Created by Andreas Reuter on 23.11.20.
+//  Copyright © 2020 NO DANCE MONKEY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IncognitoPayKit.
+FOUNDATION_EXPORT double IncognitoPayKitVersionNumber;
+
+//! Project version string for IncognitoPayKit.
+FOUNDATION_EXPORT const unsigned char IncognitoPayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IncognitoPayKit/PublicHeader.h>
+
+
